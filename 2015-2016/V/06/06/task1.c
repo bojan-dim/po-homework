b1b96cb2690228741b0fad54a6ce2b280f568f77
bojan-dim <bojan_dim@abv.bg>
2015-11-03 20:26:19 +0200

    task1.c Боян Димитров 10в номер 6 домашно 6
    A program that finds whether a number is primary or not

---
+++ 2015-2016/V/06/06/task1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int exact_square(int);
+
+int main(){
+	int n;
+	scanf("%d",&n);
+	exact_square(n);
+}
+int exact_square(int n){
+	int i,square=0;
+	for(i=0;i<n/2;i++){
+		if(n==i*i){
+			square=i;
+			break;	
+		}
+	}
+	if(square==0){
+		printf("0");
+		printf("\nThis number does not have an exact square");
+	}
+	if(square!=0){
+		printf("1");
+		printf("\n %d=%d^2",n,square);
+	}
+}
